8c5f5e1a106005fddfda1cb75baaa8dcc6c6334e
hgmk <71486402+hgmk@users.noreply.github.com>
2021-05-20 12:15:36 +0000

    Add files via upload
    first upload

---
+++ HelloFromMorrison/src/halm/Greetings.java
@@ -0,0 +1,9 @@
+package halm;
+
+public class Greetings {
+
+    public static void main(String [] arg){
+        System.out.println("Hello from Morrison!!");
+        System.out.println("Hello Everyone!!");
+    }
+}
